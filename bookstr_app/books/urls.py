ff708af11475ee5dd140ac3e4f55aa80363d7f28
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-08-06 22:20:40 +0530

    Update urls

--- bookstr_app/books/urls.py
+++ bookstr_app/books/urls.py
@@ -4,5 +4,7 @@ from books import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('<int:book_id>/', views.book_detail, name='book_detail'),
-    path('add/', views.add_book, name='add_book')
+    path('add/', views.add_book, name='add_book'),
+    path('<int:book_id>/edit/',views.edit_book,name='edit_book'),
+    path('<int:book_id>/delete/',views.delete_book,name='delete_book')
 ]
