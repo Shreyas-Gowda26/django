5cbfb686ed851206c7a325f038f949a964720c9a
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-08-10 22:22:29 +0530

    Add owner field

--- bookstr_app/books/models.py
+++ bookstr_app/books/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -8,5 +8,6 @@ class Book(models.Model):
     pages = models.IntegerField(null=True)
     image = models.URLField(null=True)
 
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.title
