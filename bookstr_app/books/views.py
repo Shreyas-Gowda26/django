88111d59a567e7c040e690f8deec3a31d40ac236
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-08-10 22:21:37 +0530

    Add permission

--- bookstr_app/books/views.py
+++ bookstr_app/books/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render,get_object_or_404,redirect
+from django.contrib.auth import login,authenticate,logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from .models import Book
 from .forms import BookForm
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import BookSerializer
+from rest_framework.decorators import api_view
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import AllowAny
+from rest_framework.authentication import BasicAuthentication
+
+@authentication_classes([BasicAuthentication])
+@permission_classes([AllowAny])
 # Create your views here.
 def home(request):
     books = Book.objects.all()
@@ -17,6 +31,7 @@ def book_detail(request, book_id):
     }
     return render(request, 'books/detail.html', context)
 
+@login_required
 def add_book(request):
 
     if request.method == "POST":
@@ -34,7 +49,7 @@ def add_book(request):
         "books/add_book.html",
         {"form": form}
     )
-
+@login_required
 def edit_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
 
@@ -50,6 +65,7 @@ def edit_book(request, book_id):
 
     return render(request, "books/edit_book.html", {"form": form})
 
+@login_required
 def delete_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
 
@@ -58,3 +74,65 @@ def delete_book(request, book_id):
         return redirect("home")
 
     return render(request, "books/delete_book.html", {"book": book})
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = User.objects.create_user(
+            username=username,
+            password=password
+        )
+
+        login(request, user)  # auto login after register
+        return redirect("home")
+
+    return render(request, "books/register.html")
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            return render(request, "books/login.html", {"error": "Invalid credentials"})
+
+    return render(request, "books/login.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect("home")
+
+@api_view(['GET', 'POST'])
+def book_list_api(request):
+
+    if request.method == 'GET':
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        print("DATA:", request.data)
+        serializer = BookSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+
+        print(serializer.errors)   # 🔥 ADD THIS
+        return Response(serializer.errors, status=400)
+    
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwner
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated, IsOwner]
